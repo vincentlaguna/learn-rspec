311b2df1594280081affde0b6ec9f3e2f8bb89a1
vincentlaguna <vincentlaguna@live.com>
2020-03-25 03:15:31 +0000

    Assingment 7 completed

--- spec/one_liner_syntax_spec.rb
+++ spec/one_liner_syntax_spec.rb
@@ -11,3 +11,15 @@ RSpec.describe 'shorthand syntax' do
       it { is_expected.to eq(5)  }
   end
 end
+
+RSpec.describe 'check array' do
+  subject { [1, 2,] }
+  
+  it 'should return an array of 1, 2' do
+    expect(subject).to eq([1, 2])
+  end
+  
+  context 'should return an array of 1, 2' do
+    it { is_expected.to eq([1, 2]) }
+  end
+end
